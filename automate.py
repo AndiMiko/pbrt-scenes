794e70fd66b736e794d09796649f9588e3f5372d
Andreas Mikolajewski <andreas.mikolajewski@gmail.com>
2018-05-13 10:32:41 +0200

    Add render automation scripts

--- automate.py
+++ automate.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python
-import json
+import json, sys, getopt, itertools, re, os, subprocess
 
-with open("../../../pbrt-scenes/extender/extenderL.pbrt", "r") as pbrtFile:
+def permuteConfig(config):
+    settingNames = []
+    settingLists = []
+    for setting in config:
+        settingNames.append(setting)
+        values = []
+        for value in config[setting]:
+            values.append(value)
+        settingLists.append(values)
+    configurations = list(itertools.product(*settingLists))
+    print(str(len(configurations)) + " configuration(s) were created.")
+    return (settingNames, configurations)
+
+def subSettingValue(name, value, pbrtFile):
+    return re.sub(r'(?P<before>' + str(name) + r'" *?[\["]{1,2})(.*?)(?P<after>[\]"]{1,2})', r'\g<before>' + str(value) + r'\g<after>', pbrtFile)
+
+def render(pbrt, args):
+    tempFile = re.sub(r'\.pbrt$', r'_temp.pbrt', args[-1])
+    assert tempFile != args[-1]
+    with open(tempFile, "w+") as pbrtFile:
+        pbrtFile.write(pbrt)
+    subprocess.call(["./pbrt"] + args[:-1] + [tempFile])
+    os.remove(tempFile)
+
+
+with open(args[-1], "r") as pbrtFile:
     pbrt = pbrtFile.read()
 
-with open("render.config", "r") as configFile:
-    configs = json.loads(configFile.read())
+with open("automate.config", "r") as configFile:
+    renderGroups = json.loads(configFile.read())
 
-for config in configs:
-    for setting in config:
-        print (setting)
-
-#from subprocess import call
-#call(["ls", "-l"])
+for renderGroup in renderGroups:
+    settingNames, configurations = permuteConfig(renderGroup)
+    for config in configurations:
+        currentFile = pbrt
+        for setting, value in zip(settingNames, config):
+            currentFile = subSettingValue(setting, value, currentFile)
+        render(currentFile, sys.argv[1:])
