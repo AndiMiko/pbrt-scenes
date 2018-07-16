a21de4492a9bda27921474d8ab5ce86c7a006191
Andreas Mikolajewski <andi_miko@yahoo.de>
2018-07-16 22:53:07 +0200

    Run mse.py for all subfolders recursively

--- mse.py
+++ mse.py
@@ -18,41 +18,48 @@ def calcMSE(curimg, refimg):
 if (len(sys.argv) != 3):
     print ("Usage: <referenceFile.exr> <check File or Folder>")
     sys.exit(0)
-refFilePath, curFilePath = sys.argv[1], sys.argv[2]
+refFilePath, tarFilePath = sys.argv[1], sys.argv[2]
 refereceFile = pyexr.open(refFilePath)
-curFilePaths = []
-workbook = xlsxwriter.Workbook(str(os.path.dirname(curFilePath) + '\mse_report.xlsx'))
-print(str('Writing report to: ' + os.path.dirname(curFilePath) + '\mse_report.xlsx'))
-worksheet = workbook.add_worksheet()
-worksheet.write(0, 0, refFilePath)
-worksheet.write(0, 1, curFilePath)
+if (os.path.isdir(tarFilePath)):
+    tarFilePaths = [x[0] for x in os.walk(tarFilePath)]
 
-startDataLine = 3
-worksheet.write(startDataLine, 0, "Filename")
-worksheet.write(startDataLine, 1, "MSE")
-worksheet.write(startDataLine, 2, "Seconds")
-worksheet.write(startDataLine, 2, "RMSE")
-worksheet.write(startDataLine, 4, "Seconds*MSE")
+for curFilePath in tarFilePaths:
+    if not curFilePath.endswith('.exr'):
+        curFilePath += '\\'
+    curFilePaths = []
 
-if (os.path.isdir(curFilePath)):
-    for filename in glob.iglob(curFilePath + '*.exr'):
-        curFilePaths.append(filename)
-else:
-    curFilePaths.append(curFilePath)
+    if (os.path.isdir(curFilePath)):
+        for filename in glob.iglob(curFilePath + '*.exr'):
+            curFilePaths.append(filename)
+    else:
+        curFilePaths.append(curFilePath)
+
+    if curFilePaths:
+        workbook = xlsxwriter.Workbook(str(os.path.dirname(curFilePath) + '\\' + os.path.dirname(curFilePath) + '_mse_report.xlsx'))
+        print(str('Writing report to: ' + os.path.dirname(curFilePath) + '\\' + os.path.dirname(curFilePath) + '_mse_report.xlsx'))
+        worksheet = workbook.add_worksheet()
+        worksheet.write(0, 0, refFilePath)
+        worksheet.write(0, 1, curFilePath)
+
+        startDataLine = 3
+        worksheet.write(startDataLine, 0, "Filename")
+        worksheet.write(startDataLine, 1, "MSE")
+        worksheet.write(startDataLine, 2, "Seconds")
+        worksheet.write(startDataLine, 2, "RMSE")
+        worksheet.write(startDataLine, 4, "Seconds*MSE")
+
+        for index, curFilePath in enumerate(curFilePaths, start=startDataLine+1):
+            print("\rCalculating file %d/%d... [%s]" % (index - startDataLine, len(curFilePaths), curFilePath)),
+            stdout.flush()
+            worksheet.write(index, 0, curFilePath)
+            mse = calcMSE(pyexr.open(curFilePath).get(), refereceFile.get())
+            worksheet.write(index, 1, mse)
+            worksheet.write(index, 3, math.sqrt(mse))
+            time = re.search(r'_t(\d+)_', curFilePath, re.IGNORECASE)
+            if time:
+                seconds = int(time.group(1))
+                worksheet.write(index, 2, seconds)
+                worksheet.write(index, 4, seconds*mse)
 
 
-for index, curFilePath in enumerate(curFilePaths, start=startDataLine+1):
-    print("\rCalculating file %d/%d... [%s]" % (index - startDataLine, len(curFilePaths), curFilePath)),
-    stdout.flush()
-    worksheet.write(index, 0, curFilePath)
-    mse = calcMSE(pyexr.open(curFilePath).get(), refereceFile.get())
-    worksheet.write(index, 1, mse)
-    time = re.search(r'_t(\d+)_', curFilePath, re.IGNORECASE)
-    if time:
-        seconds = int(time.group(1))
-        worksheet.write(index, 2, seconds)
-        worksheet.write(index, 3, math.sqrt(seconds))
-        worksheet.write(index, 4, seconds*mse)
-
-
-workbook.close()
+        workbook.close()
